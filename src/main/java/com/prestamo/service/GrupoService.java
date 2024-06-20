9bd03b893853a48409cb390caa7730c4127f5dd7
PatrickG2003 <patrickgr26112003@gmail.com>
2024-06-20 17:56:05 -0500

    Avance Crud Grupo

--- src/main/java/com/prestamo/service/GrupoService.java
+++ src/main/java/com/prestamo/service/GrupoService.java
@@ -6,9 +6,14 @@ import com.prestamo.entity.Grupo;
 
 public interface GrupoService {
 	
-	public abstract Grupo insertaActualizaGrupo(Grupo obj);
-	public abstract List<Grupo> listaGrupo();
-	public abstract List<Grupo> listaGrupoPorDescripcionIgual(String descripcion);
 	
+		public abstract List<Grupo> listaGrupoPorDescripcionIgual(String descripcion);
+	
+	//PC2 CRUD
+		public abstract Grupo insertaActualizaGrupo(Grupo obj);
+		public abstract List<Grupo> listaGrupo();
+		public abstract List<Grupo> listaGrupoPorDescripcionLike(String nombre);
+		public abstract void eliminaGrupo(int idRevista);
+		
 
 }
