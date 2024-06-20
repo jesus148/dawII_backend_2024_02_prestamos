c6762d2224490036ae220ee9add58aa5910a68e0
DanielPRe15 <dani123pre@gmail.com>
2024-06-19 21:07:11 -0500

    PC2-CRUD-Entidad-Financiera

--- src/main/java/com/prestamo/service/EntidadFinancieraService.java
+++ src/main/java/com/prestamo/service/EntidadFinancieraService.java
@@ -2,14 +2,20 @@ package com.prestamo.service;
 
 import java.util.List;
 
-
 import com.prestamo.entity.EntidadFinanciera;
 
 public interface EntidadFinancieraService {
-
-	public abstract EntidadFinanciera insertaActualizaEntidadFinanciera(EntidadFinanciera obj);
-	public abstract List<EntidadFinanciera> listaEntidadFinanciera();
+	
+	//Validaciones 
 	public abstract List<EntidadFinanciera> listaEntidadFinancieraPorNombreIgual(String nombre);
+	public abstract List<EntidadFinanciera> listaEntidadFinancieraPorDescripcionIgualActualiza(String nombre, int idEntidadFinanciera);
+	
+	
+	//CRUD
+	public abstract EntidadFinanciera insertaActualizaEntidadFinanciera(EntidadFinanciera obj);
+	public abstract List<EntidadFinanciera> listaEntidadFinancieraPorDescripcionLike(String nombre);
+	public abstract void eliminaEntidadFinanciera(int idRevista);
+	public abstract List<EntidadFinanciera> listaEntidadFinanciera();
 	public abstract List<EntidadFinanciera> listarEntidadPorTipo(int id);
 
 }
